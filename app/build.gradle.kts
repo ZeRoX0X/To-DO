acb4734a6930d08c66ad932d69fcc4b4e4dbe293
ZeRoX0X <97478111+ZeRoX0X@users.noreply.github.com>
2023-11-23 18:59:20 +0300

    Changing the compileSdk & targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.todo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.todo"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
