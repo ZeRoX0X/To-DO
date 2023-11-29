f019ca3d18d12957edd68928791b0f385ec53927
ZeRoX0X <97478111+ZeRoX0X@users.noreply.github.com>
2023-11-29 11:41:56 +0300

    1-refactoring (changed the names of the fragments) 2-Changed the TargetApi in the Manifest file to 34

--- app/src/main/java/com/example/todo/ui/tasks/TasksViewModel.java
+++ app/src/main/java/com/example/todo/ui/tasks/TasksViewModel.java
@@ -1,16 +1,16 @@
-package com.example.todo.ui.gallery;
+package com.example.todo.ui.tasks;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class TasksViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public TasksViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is tasks fragment");
     }
 
     public LiveData<String> getText() {
