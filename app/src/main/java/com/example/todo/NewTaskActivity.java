0576cdf945be0332bd1f3e73ffcbc3313e06a394
abeeraasee <abeer.afragrance@gmail.com>
2023-12-05 09:52:56 +0300

    new appointment layout date picker time picker

--- app/src/main/java/com/example/todo/NewTaskActivity.java
+++ app/src/main/java/com/example/todo/NewTaskActivity.java
@@ -1,12 +1,25 @@
 package com.example.todo;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.todo.databinding.ActivityNewTaskBinding;
 
 public class NewTaskActivity extends AppCompatActivity {
+    // New ---
+    private TextView textDate;
+    private Button date;
+    private TextView textTime;
+    private Button time;
+
 
     private ActivityNewTaskBinding binding;
 
@@ -26,6 +39,58 @@ public class NewTaskActivity extends AppCompatActivity {
             // Call the method on the action bar
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // New ---
+        textDate = findViewById(R.id.AppointmentDate);
+        date = findViewById(R.id.AppointmentDateButton);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDialogDate();
+
+            }
+        });
+
+        textTime = findViewById(R.id.AppointmentTime);
+        time = findViewById(R.id.AppointmentTimeButton);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+
+    // New ---
+    private void openDialogDate(){
+        DatePickerDialog dialogDate = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                textDate.setText(String.valueOf(year)+" , "+String.valueOf(month+1)+" , "+String.valueOf(day));
+            }
+        }, 2023, 0, 10);
+
+        dialogDate.show();
+    }
+
+    private void openDialogTime(){
+
+        TimePickerDialog dialogTime = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+
+                textTime.setText(String.valueOf(hours)+":"+String.valueOf(minutes));
+
+            }
+        }, 10, 00, true);
+
     }
 }
 
