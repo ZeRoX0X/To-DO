e7ccde3efdebfe9fc0697f8cc61c92570635af25
ZeRoX0X <97478111+ZeRoX0X@users.noreply.github.com>
2023-12-15 23:26:07 +0300

    AppointmentAdapter update.

--- app/src/main/java/com/example/todo/ui/AppointmentAdapter.java
+++ app/src/main/java/com/example/todo/ui/AppointmentAdapter.java
@@ -2,17 +2,24 @@ package com.example.todo.ui;
 
 // Import the required libraries
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todo.R;
 import com.example.todo.data.models.Appointment;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
+
 // Create a class that extends RecyclerView.Adapter and uses a custom ViewHolder class
 public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.AppointmentViewHolder> {
     // Declare a private field to store the list of appointments
@@ -24,19 +31,68 @@ public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.
         this.context = context;
         this.appointments = appointments;
     }
+    // Define constants for the view types
+    private static final int DONE = 1;
+    private static final int CANCELLED = 2;
+    private static final int DELAYED = 3;
+
+    // Override the getItemViewType method to return the view type based on the appointment status
+    @Override
+    public int getItemViewType(int position) {
+        // Get the appointment object at the given position
+        Appointment appointment = appointments.get(position);
+
+        // Check the status of the appointment and return the corresponding view type
+        if (appointment.getStat().equals("completed")) {
+            return DONE;
+        } else if (appointment.getStat().equals("canceled")) {
+            return CANCELLED;
+        } else if (appointment.getStat().equals("delayed")) {
+            return DELAYED;
+        }
+
+        else {
+            // If the status is not done or cancelled, return a default view type
+            return super.getItemViewType(position);
+        }
+    }
+
 
     // Override the onCreateViewHolder method to inflate the item layout and return a new ViewHolder object
+    // Override the onCreateViewHolder method to inflate the item layout and return a new ViewHolder object
+    @NonNull
     @Override
     public AppointmentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // Get a layout inflater from the context
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        // Inflate the item layout using the layout inflater
-        View itemView = inflater.inflate(R.layout.appointment_item, parent, false);
+        // Declare a view variable to store the item view
+        View itemView;
+
+        // Use a switch statement to check the view type and inflate the corresponding layout
+        switch (viewType) {
+            case DONE:
+                // Inflate the layout for done appointments
+                itemView = inflater.inflate(R.layout.event_item_done, parent, false);
+                break;
+            case CANCELLED:
+                // Inflate the layout for cancelled appointments
+                itemView = inflater.inflate(R.layout.event_item_cancelled, parent, false);
+                break;
+            case DELAYED:
+                // Inflate the layout for cancelled appointments
+                itemView = inflater.inflate(R.layout.event_item_delayed, parent, false);
+                break;
+            default:
+                // Inflate the default layout
+                itemView = inflater.inflate(R.layout.event_item_pending, parent, false);
+                break;
+        }
 
         // Return a new ViewHolder object with the item view
         return new AppointmentViewHolder(itemView);
     }
+
 
     // Override the onBindViewHolder method to bind the data to the views in the ViewHolder object
     @Override
@@ -44,17 +100,89 @@ public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.
         // Get the appointment object at the given position
         Appointment appointment = appointments.get(position);
 
-        // Set the text of the views in the ViewHolder object with the data from the appointment object
-        holder.textViewId.setText(String.valueOf(appointment.getId()));
-        holder.textViewDatetime.setText(appointment.getStart_datetime());
-        holder.textViewReminder.setText(appointment.getReminder_datetime());
-        holder.textViewStat.setText(appointment.getStat());
-        holder.textViewAgentName.setText(appointment.getAgent_name());
-        holder.textViewAgentCategory.setText(appointment.getAgent_category());
-        holder.textViewAgentPhone.setText(appointment.getAgent_phone());
-        holder.textViewDependencyName.setText(appointment.getDependency_name());
-        holder.textViewDependencyCategory.setText(appointment.getDependency_category());
+        // Get the view type of the item
+        int viewType = getItemViewType(position);
+        // Set the text of the views for default appointments
+        String start_datetime = appointment.getStart_datetime();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime ldt = LocalDateTime.parse(start_datetime, dtf);
+
+
+        String monthName = ldt.getMonth().getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+        String dayName = ldt.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+        int dayNumber = ldt.getDayOfMonth();
+
+
+        // Use a switch statement to check the view type and bind the data accordingly
+        switch (viewType) {
+            case DONE:
+                // Set the text of the views for done appointments
+
+
+                holder.textViewMonthName.setText(monthName);
+                holder.textViewDayName.setText(dayName);
+                holder.textViewDayNumber.setText(String.valueOf(dayNumber));
+                holder.textViewStat.setText(appointment.getStat());
+                holder.textViewAgentName.setText(appointment.getAgent_name());
+                holder.textViewAgentCategory.setText(appointment.getAgent_category());
+
+                holder.textViewDependencyName.setText(appointment.getDependency_name());
+                holder.textViewDependencyCategory.setText(appointment.getDependency_category());
+
+                // Set the color of the views for done appointments
+
+
+                holder.textViewAgentName.setTextColor(Color.GREEN);
+                holder.textViewAgentCategory.setTextColor(Color.GREEN);
+
+                holder.textViewDependencyName.setTextColor(Color.GREEN);
+                holder.textViewDependencyCategory.setTextColor(Color.GREEN);
+
+                // Set the icon of the views for done appointments
+                //holder.imageViewStat.setImageResource(R.drawable.ic_done);
+                break;
+            case CANCELLED:
+                // Set the text of the views for cancelled appointments
+
+
+
+                holder.textViewMonthName.setText(monthName);
+                holder.textViewDayName.setText(dayName);
+                holder.textViewDayNumber.setText(String.valueOf(dayNumber));
+                holder.textViewStat.setText(appointment.getStat());
+                holder.textViewAgentName.setText(appointment.getAgent_name());
+                holder.textViewAgentCategory.setText(appointment.getAgent_category());
+
+                holder.textViewDependencyName.setText(appointment.getDependency_name());
+                holder.textViewDependencyCategory.setText(appointment.getDependency_category());
+
+                // Set the color of the views for cancelled appointments
+
+                holder.textViewAgentName.setTextColor(Color.RED);
+                holder.textViewAgentCategory.setTextColor(Color.RED);
+
+                holder.textViewDependencyName.setTextColor(Color.RED);
+                holder.textViewDependencyCategory.setTextColor(Color.RED);
+
+                // Set the icon of the views for cancelled appointments
+                //holder.imageViewStat.setImageResource(R.drawable.ic_cancelled);
+                break;
+            default:
+
+
+                holder.textViewMonthName.setText(monthName);
+                holder.textViewDayName.setText(dayName);
+                holder.textViewDayNumber.setText(String.valueOf(dayNumber));
+                holder.textViewStat.setText(appointment.getStat());
+                holder.textViewAgentName.setText(appointment.getAgent_name());
+                holder.textViewAgentCategory.setText(appointment.getAgent_category());
+
+                holder.textViewDependencyName.setText(appointment.getDependency_name());
+                holder.textViewDependencyCategory.setText(appointment.getDependency_category());
+                break;
+        }
     }
+
     // Override the getItemCount method to return the size of the list of appointments
     @Override
     public int getItemCount() {
@@ -71,9 +199,10 @@ public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.
     // Create a nested class that extends RecyclerView.ViewHolder and holds the views for each item
     public static class AppointmentViewHolder extends RecyclerView.ViewHolder {
         // Declare the views for each item
-        TextView textViewId;
-        TextView textViewDatetime;
-        TextView textViewReminder;
+        TextView textViewMonthName;
+        TextView textViewDayName;
+        TextView textViewDayNumber;
+
         TextView textViewStat;
         TextView textViewAgentName;
         TextView textViewAgentCategory;
@@ -84,15 +213,15 @@ public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.
         public AppointmentViewHolder(View itemView) {
             super(itemView);
             // Find the views by their ids
-            textViewId = itemView.findViewById(R.id.text_view_id);
-            textViewDatetime = itemView.findViewById(R.id.text_view_datetime);
-            textViewReminder = itemView.findViewById(R.id.text_view_reminder);
-            textViewStat = itemView.findViewById(R.id.text_view_stat);
-            textViewAgentName = itemView.findViewById(R.id.text_view_agent_name);
-            textViewAgentCategory = itemView.findViewById(R.id.text_view_agent_category);
-            textViewAgentPhone = itemView.findViewById(R.id.text_view_agent_phone);
-            textViewDependencyName = itemView.findViewById(R.id.text_view_dependency_name);
-            textViewDependencyCategory = itemView.findViewById(R.id.text_view_dependency_category);
+            textViewMonthName = itemView.findViewById(R.id.monthName);
+            textViewDayName = itemView.findViewById(R.id.dayName);
+            textViewDayNumber = itemView.findViewById(R.id.dayNumber);
+            textViewStat = itemView.findViewById(R.id.status);
+            textViewAgentName = itemView.findViewById(R.id.agent_name);
+            textViewAgentCategory = itemView.findViewById(R.id.agent_category);
+            textViewAgentPhone = itemView.findViewById(R.id.agent_phone);
+            textViewDependencyName = itemView.findViewById(R.id.dependency_name);
+            textViewDependencyCategory = itemView.findViewById(R.id.dependency_category);
         }
     }
 }
